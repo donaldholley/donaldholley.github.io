4db5d8fe9168924ccadb5daeadd5938724d38b03
Donald Holley <45888265+donaldholley@users.noreply.github.com>
2018-12-19 20:58:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var arr = document.getElementsByTagName("p");
+//var arr = document.getElementsByTagName("p");
 
-for (var x = 0; x < arr.length; x++) {
-  arr[x].innerHTML = "HELLO";
-}
+//for (var x = 0; x < arr.length; x++) {
+//  arr[x].innerHTML = "HELLO";
+//}
 
-var arr2 = document.getElementsByTagName("title");
+//var arr2 = document.getElementsByTagName("title");
 
-for (var x = 0; x < arr2.length; x++) {
-  arr2[x].innerHTML = "HELLO";
+//for (var x = 0; x < arr2.length; x++) {
+//  arr2[x].innerHTML = "HELLO";
+//}
+
+var n = document.getElementById("name");
+n.onmouseover = function() {
+  n.innerHTML = "HELLO";
 }
