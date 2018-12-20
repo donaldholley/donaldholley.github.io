67543285f87bb67b549743b01a2da7d0f3146b9d
Donald Holley <45888265+donaldholley@users.noreply.github.com>
2018-12-19 20:17:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var x = document.getElementById("name");
-x.onmouseover = function() {
-  document.innerHTML = Date();
+var arr = document.getElementByTagName("p");
+
+for (var x = 0; x < arr.length; x++) {
+  arr[x].innerHTML = "HELLO";
 }
