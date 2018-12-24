1ed28e8f2340b9c5593dcc33252f168b8327d965
Donald Holley <45888265+donaldholley@users.noreply.github.com>
2018-12-23 20:09:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ n.onmouseover = function() {
   n.style.animationPlayState = "running";
   console.log("*");
 }
-n.onmounseout = function {
+n.onmounseout = function() {
   n.style.animationPlayState = "paused";
   console.log("out");
 }
